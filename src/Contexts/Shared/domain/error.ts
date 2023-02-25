bf1b0b6c7fb8e036875ddebd25c5aeab7e8a2e64
David Gallardo <davidgj1994@gmail.com>
2023-02-25 17:48:05 +0100

    feat: express api skeleton

---
+++ src/Contexts/Shared/domain/error.ts
@@ -0,0 +1,29 @@
+export interface IError {
+  type: string;
+  message: string;
+  error: any;
+}
+
+export class NotFoundError implements IError {
+  readonly type = "NotFound";
+  message: string;
+  constructor(public error: Error) {
+    this.message = error.message;
+  }
+}
+
+export class UnexpectedError implements IError {
+  readonly type = "UnexpectedError";
+  message: string;
+  constructor(public error: Error) {
+    this.message = error.message;
+  }
+}
+
+export class BadRequestError implements IError {
+  readonly type = "BadRequestError";
+  message: string;
+  constructor(public error: Error) {
+    this.message = error.message;
+  }
+}
