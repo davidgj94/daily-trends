1a59e66c3c0b844684ebbef4415109586f713687
David Gallardo <davidgj1994@gmail.com>
2023-02-26 18:17:27 +0100

    tests: several tests * updates existing Feed items in mongodb * finds by domain id * returns NotFound error if feedItem doesn't exist

--- tests/mongoFeedItem.test.ts
+++ tests/mongoFeedItem.test.ts
@@ -7,7 +7,7 @@ import { idGenerator } from "Shared/infra/idGenerator";
 const mongoRepository = new MongoFeedItemsRepository();
 
 describe("Feed mongo repository integration tests", () => {
-  it("saves domain Feed items in mongodb", async () => {
+  it("saves domain new Feed items in mongodb", async () => {
     const feedItem = new FeedItem(
       idGenerator(),
       faker.internet.url(),
@@ -20,5 +20,45 @@ describe("Feed mongo repository integration tests", () => {
     expect(saveResult.isSuccess()).toBe(true);
     const savedFeedItem = saveResult.value;
     expect(savedFeedItem).toEqual(feedItem);
+  });
+
+  it("updates existing Feed items in mongodb", async () => {
+    const feedItem = new FeedItem(
+      idGenerator(),
+      faker.internet.url(),
+      "elMundo",
+      [faker.internet.url()],
+      new Date(),
+      faker.lorem.paragraph()
+    );
+    let saveResult = await mongoRepository.save(feedItem);
+    expect(saveResult.isSuccess()).toBe(true);
+
+    feedItem.description = faker.lorem.paragraph();
+    saveResult = await mongoRepository.save(feedItem);
+    expect(saveResult.isSuccess()).toBe(true);
+    expect(saveResult.value).toEqual(feedItem);
+  });
+
+  it("finds by domain id", async () => {
+    const feedItem = new FeedItem(
+      idGenerator(),
+      faker.internet.url(),
+      "elMundo",
+      [faker.internet.url()],
+      new Date(),
+      faker.lorem.paragraph()
+    );
+    let saveResult = await mongoRepository.save(feedItem);
+    expect(saveResult.isSuccess()).toBe(true);
+    const findResult = await mongoRepository.findbyId(feedItem.id);
+    expect(findResult.isSuccess()).toBe(true);
+    expect(findResult.value).toEqual(feedItem);
+  });
+
+  it("returns NotFound error if feedItem doesn't exist", async () => {
+    const findResult = await mongoRepository.findbyId(idGenerator());
+    expect(findResult.isFailure()).toBe(true);
+    expect(findResult.error.type === "NotFound").toBe(true);
   });
 });
