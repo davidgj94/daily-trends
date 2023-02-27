bb0ea5f1827416bee128dd622e6ed1ef8d29110d
David Gallardo <davidgj1994@gmail.com>
2023-02-27 09:44:14 +0100

    refactor: delete error handler

--- src/apps/api/controllers/getFeedController.ts
+++ src/apps/api/controllers/getFeedController.ts
@@ -1,13 +1,23 @@
 import { RequestHandler } from "express";
 import { ItemSource } from "Feed/domain/feedItem";
+import { StatusCodes } from "http-status-codes";
 import { getFeedUseCase } from "../dependencyInjection";
 
-export const getFeedController: RequestHandler = async (req, res, next) => {
+export const getFeedController: RequestHandler = async (req, res) => {
   const date = req.query.date ? new Date(req.query.date as string) : new Date();
   const selectedSources = req.query.sources as ItemSource[];
   const getFeedResult = await getFeedUseCase.execute({ date, selectedSources });
   if (getFeedResult.isFailure()) {
-    next(getFeedResult.error);
+    switch (getFeedResult.error.type) {
+      case "NotFound":
+        res.status(StatusCodes.NOT_FOUND).json(getFeedResult.error.message);
+        break;
+      default:
+        res
+          .status(StatusCodes.INTERNAL_SERVER_ERROR)
+          .json(getFeedResult.error.message);
+        break;
+    }
     return;
   }
   res.json(getFeedResult.value);
