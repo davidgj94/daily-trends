619399cdf1a65be557e26a9fd4ca5941d0cb2a13
David Gallardo <davidgj1994@gmail.com>
2023-02-27 19:26:47 +0100

    refactor: scrape different webs in pararell

--- src/apps/scraper/index.ts
+++ src/apps/scraper/index.ts
@@ -1,26 +1,9 @@
 import { startDB } from "Shared/infra/persistence/mongo/init";
-import { createItemUseCase } from "../api/dependencyInjection";
-import { elPaisScraperJob } from "./dependencyInjection";
-import { ItemSource } from "Feed/domain/feedItem";
-import { CreateItemRequest } from "Feed/application/createFeedItem";
-import { ScraperJob } from "./scraperJob";
+import { elMundoScraperJob, elPaisScraperJob } from "./dependencyInjection";
 
-const SCRAPER_JOBS = new Map<ItemSource, ScraperJob>();
-SCRAPER_JOBS.set("elPais", elPaisScraperJob);
+const SCRAPER_JOBS = [elMundoScraperJob, elPaisScraperJob];
 
 startDB().then(async () => {
-  const items: CreateItemRequest[] = [];
-  for (const [source, job] of SCRAPER_JOBS.entries()) {
-    const results = await job.run();
-    items.push(...results.map((data) => ({ ...data, source })));
-  }
-
-  console.log(items);
-  await Promise.all(
-    items.map(async (item) => {
-      const response = await createItemUseCase.execute(item);
-      if (response.isFailure()) console.error(response.error);
-    })
-  );
+  await Promise.all(SCRAPER_JOBS.map(async (job) => await job.run()));
   console.log("Done");
 });
